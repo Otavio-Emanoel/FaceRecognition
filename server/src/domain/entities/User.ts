a3056db75f995598324cc105cf373f3d91379f06
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2026-03-14 23:28:56 -0300

    feat: criar classes User e UserRepository para gerenciamento de usuários

---
+++ server/src/domain/entities/User.ts
@@ -0,0 +1,17 @@
+class User {
+    constructor(
+        public id: string,
+        public name: string,
+        public email: string,
+        public password: string,
+        public descriptor: number[]
+    ) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.descriptor = descriptor;
+    }
+}
+
+module.exports = User;
---
+++ server/src/domain/repositories/UserRepository.ts
@@ -0,0 +1,9 @@
+class UserRepository {
+    async save() {
+        throw new Error('Method not implemented.')
+    }
+
+    async findAll() {
+        throw new Error('Method not implemented.')
+    }
+}
